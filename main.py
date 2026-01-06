ea593a5725512b982460329e5c7265ec29554aa0
jellalis <ppanag94@gmail.com>
2026-01-06 02:16:11 +0200

    first commit

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from llama_index.core.query_pipeline import QueryPipeline
 from prompts import context,code_parser_template
 from code_reader import code_reader
 import ast
+import os
 load_dotenv()
 
 llm=Ollama(
@@ -57,11 +58,33 @@ json_prompt_str=parser.format(code_parser_template)
 json_prompt_tmpl=PromptTemplate(json_prompt_str)
 output_pipeline=QueryPipeline(chain=[json_prompt_tmpl,llm])
 while(prompt:=input('Enter a prompt(q to quit :'))!='q':
-    result=agent.query(prompt)
-    next_result=output_pipeline.run(response=result)
-    cleaned_json=ast.literal_eval(str(next_result).replace("assistant:",""))
+    retries = 0#retry hanler (part (5)
+
+    while retries <3 :#retry hanler (part (5)
+        try:#retry hanler (part (5)
+            result=agent.query(prompt)
+            next_result=output_pipeline.run(response=result)
+            cleaned_json=ast.literal_eval(str(next_result).replace("assistant:",""))
+            break#retry hanler (part (5)
+        except Exception as e:#retry hanler (part (5)
+            retries+=1#retry hanler (part (5)
+            print(f'Error occured, retry #{retries}',e)#retry hanler (part (5)
+    if retries>=3:#retry hanler (part (5)
+        print ('unable to process request,try again...')#retry hanler (part (5)
+        continue#retry hanler (part (5)
+
+
     print('code generated')
     print(cleaned_json['code'])
 
     print('n\nDescritption',cleaned_json['description'])
     filename=cleaned_json['filename']
+    try :#saving to a file (part(6))
+        with open(os.path.join('output',filename),'w') as f:#helps not to overwite something cause i created a file output and there i will put everything
+            f.write(cleaned_json['code'])
+            print('saved file',filename)
+    except:
+        print('error saving file')
+
+
+
